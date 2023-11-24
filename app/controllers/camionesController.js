1914d103ee834c66fd58b92d8777330e315b65c6
Fabricio Arrua <fabricio.arrua03@gmail.com>
2023-11-24 14:43:45 -0300

    ajustes tildes

--- app/controllers/camionesController.js
+++ app/controllers/camionesController.js
@@ -18,12 +18,12 @@ module.exports = {
     
             if(results[0].length == 0) {
               return res.status(200).json( {
-                message: 'No existe dicho camion'
+                message: 'No existe dicho camión'
               })
             }
             else{
               return res.status(200).json( {
-                message: 'Existe dicho camion',
+                message: 'Existe dicho camión',
                 listado: results[0],
               })
             }
@@ -71,12 +71,12 @@ module.exports = {
 
         if(results[0].length == 0) {
             return res.status(200).json( {
-            message: 'No existen camiones en reparacion'
+            message: 'No existen camiones en reparación'
             })
         }
         else{
             return res.status(200).json( {
-            message: 'Existen camiones en reparacion',
+            message: 'Existen camiones en reparación',
             listado: results[0],
             })
         }
@@ -168,7 +168,7 @@ module.exports = {
             resultado = results[0].resultado;
 
             if (resultado == null){
-                resultado = 'Alta realizada con exito';
+                resultado = 'Alta realizada con éxito';
             }
             // Cierra la conexión
             return res.status(200).json( {
@@ -211,7 +211,7 @@ module.exports = {
             resultado = results[0].resultado;
 
             if (resultado == null){
-                resultado = 'Modificacion realizada con exito';
+                resultado = 'Modificación realizada con éxito';
             }
             // Cierra la conexión
             return res.status(200).json( {
@@ -248,7 +248,7 @@ module.exports = {
             resultado = results[0].resultado;
 
             if (resultado == null){
-                resultado = 'Baja realizada con exito';
+                resultado = 'Baja realizada con éxito';
             }
             // Cierra la conexión
             return res.status(200).json( {
