8175760c7bfcc72b6e751f44f73b136af6f98ed0
Diego <diegomsg@hotmail.com>
2024-01-29 21:17:24 -0300

    creacion bd mongo

--- app/mongoDB.js
+++ app/mongoDB.js
@@ -5,9 +5,25 @@ const mongoConexion = new MongoClient(uri);
 
 mongoConexion.connect((err) => {
     if (err) {
-      console.log("Error al conectar a MongoDB", err);
+        console.log("Error al conectar a MongoDB", err);
     } else {
-      console.log("Conexión a MongoDB exitosa");
+        console.log("Conexión a MongoDB exitosa");
+
+        // Seleccionar la base de datos
+        const miBaseDeDatos = mongoConexion.db('transportesdb');
+
+        // Intentar acceder a la colección (esto la crea si no existe)
+        miBaseDeDatos.createCollection('ubicaciones', (err, result) => {
+            if (err) {
+                console.log("Error al crear la colección", err);
+            } else {
+                console.log("Colección creada correctamente");
+            }
+            
+            // Cerrar la conexión después de realizar operaciones
+            mongoConexion.close();
+        });
     }
 });
+
 module.exports = mongoConexion;
